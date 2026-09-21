8c3af2567019485afe3b0ba2f926d7a61b8a5c9f
Sparsa <sparsaww@gmail.com>
2026-09-21 16:02:01 +0545

    Revert "feat: add PushAlert push notification script to layout"
    This reverts commit f0f7b141d14d623c913523c3b73d85925fc8a11b.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import Script from 'next/script';
 import MobileBottomNav from '@/components/MobileBottomNav';
 import { UserProvider } from '@/context/UserContext';
 import './globals.css';
@@ -21,11 +20,6 @@ export default function RootLayout({
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
-        />
-        {/* PushAlert Unified Code */}
-        <Script
-          src="https://cdn.pushalert.co/unified_76773fc185ec5b697df65486a3168cd3.js"
-          strategy="beforeInteractive"
         />
       </head>
       <body className="font-sans bg-[#0B0E14] text-gray-100 antialiased pb-16 md:pb-0">
